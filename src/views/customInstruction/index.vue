f8033cd3cfcd79dd0592a21da455f0efa5c95fbe
Guomin-Han <2371096524@qq.com>
2021-11-22 22:51:44 +0800

    feat(customInstruction\index.vue):添加自定义指令页面

---
+++ src/views/customInstruction/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dialog">
+    <input v-model="value" v-anti-shake="getData" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomInstruction',
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    getData() {
+      console.log(this.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
